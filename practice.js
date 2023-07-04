3be973a9eab6fe40913e4f17661bd7b6fcb91ead
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2023-07-04 14:45:37 -0700

    solved leetcode #860 binary gap

--- practice.js
+++ practice.js
@@ -6,34 +6,68 @@ Note that you do not have any change in hand at first.
 
 Given an integer array bills where bills[i] is the bill the ith customer pays, return true if you can provide every customer with the correct change, or false otherwise.
 */
-var lemonadeChange = function(bills) {
-    //establish counter variables to keep track of the number of $5 and $10 bills
-    let number_5 =0;
-    let number_10 = 0;
-    //iterate through the bills array
-    for (let i of bills){
-    //for anyone paying the exact $5, increment the number of $5 bills by one
-        if (i==5){
-            number_5++;
-        }
-        //if one has a $10 bill, will need to decrement $5 by one and increment $10 bill by one (checking first to see if we have a $5 billl)
-        else if (i==10){
-            if(number_5 == 0) return false;
-            number_5--, number_10++;
-        }
-        //if the number of $10 and $5 bills is greater than 0 (someone pays with a $20), will need to decrement both 5 and 10
-        // else if the number of $5 bills is greater than 3, then one can give change with three $5s
-         else {
-                if (number_5 > 0 && number_10 > 0) {
-                    number_5--;
-                    number_10--;
-                } else if (number_5 >= 3) {
-                    number_5 -= 3;
-                } else {
-                    return false;
-                }
-            }
-        }
-// default
-        return true;
-    }
+// var lemonadeChange = function(bills) {
+//     //establish counter variables to keep track of the number of $5 and $10 bills
+//     let number_5 =0;
+//     let number_10 = 0;
+//     //iterate through the bills array
+//     for (let i of bills){
+//     //for anyone paying the exact $5, increment the number of $5 bills by one
+//         if (i==5){
+//             number_5++;
+//         }
+//         //if one has a $10 bill, will need to decrement $5 by one and increment $10 bill by one (checking first to see if we have a $5 billl)
+//         else if (i==10){
+//             if(number_5 == 0) return false;
+//             number_5--, number_10++;
+//         }
+//         //if the number of $10 and $5 bills is greater than 0 (someone pays with a $20), will need to decrement both 5 and 10
+//         // else if the number of $5 bills is greater than 3, then one can give change with three $5s
+//          else {
+//                 if (number_5 > 0 && number_10 > 0) {
+//                     number_5--;
+//                     number_10--;
+//                 } else if (number_5 >= 3) {
+//                     number_5 -= 3;
+//                 } else {
+//                     return false;
+//                 }
+//             }
+//         }
+// // default
+//         return true;
+//     }
+
+/*#868 Binary Gap:
+Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n.
+If there are no two adjacent 1's, return 0.
+Two 1's are adjacent if there are only 0's separating them (possibly no 0's).
+The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
+*/
+
+var binaryGap = function (n) {
+    // convert number to binary with .toString(base 2) method
+	let binary = n.toString(2);
+	console.log("binary", binary); //10110
+	let longest = 0,
+		last = 0;
+	    i = 0;
+    //iterate through the binary number
+	while (i < binary.length) {
+        //check if index is equal to 1
+		if (binary[i] === "1") {
+            //calculate the distance between the current index of 1 and the last index of 1
+			let distance = i - last;
+            //set last to the index if it's equal to 1
+			last = i;
+            //get the longest distance between the longest and the distance, set it equal to longest
+			longest = Math.max(longest, distance);
+		}
+        // increment i
+		i++;
+	}
+    //return the longest var
+	return longest;
+};
+
+console.log(binaryGap(22));
