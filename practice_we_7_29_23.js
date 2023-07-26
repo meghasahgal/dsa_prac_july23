05497e76aae4c86a4bebac3e50678e1e33251310
Megha Sahgal <meghas@gmail.com>
2023-07-26 14:36:52 -0700

    solved #51

--- practice_we_7_29_23.js
+++ practice_we_7_29_23.js
@@ -39,42 +39,83 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+// /**
+//  * @param {ListNode} head
+//  * @param {number} n
+//  * @return {ListNode}
+//  */
+
+// var removeNthFromEnd = function (head, n) {
+// 	//create a dummy node pointer for node before the head node (use dummy in adding or deleting a node)
+// 	let dummyHead = new ListNode(-1);
+//     console.log(dummyHead, "dummyHead")
+// 	// create a head node
+// 	dummyHead.next = head;
+// 	let resultHead = dummyHead;
+//     console.log(resultHead, "resultHead")
+// 	//create a counter to find the correct nth node
+// 	let counter = 0;
+//     //create a tail poiinter that's set to the head
+// 	let tail = head;
+//     console.log(tail, "tail")
+// 	while (counter < n) {
+//         counter++
+// 		//increment the tail while counter < n
+// 		tail = tail.next;
+// 	}
+//     //create a removedNode pointer that's set to head as well. when you get to n, remove the head
+// 	let removedNode = head;
+// 	let prev = dummyHead;
+//     //increment 3 pointers until tail = null and see where it lands
+// 	while (tail) {
+// 		tail = tail.next;
+//         removedNode = removedNode.next;
+//         prev = prev.next;
+// 	}
+//     prev.next = removedNode.next;
+//     return resultHead.next;
+// };
+// //time complex: O(n);space O(1)
+
+// console.log(removeNthFromEnd([1, 2, 3, 4, 5], 2));
+// //--------
+// // #56. Merge Intervals
+
+// // Companies
+// // Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals,
+// // and return an array of the non-overlapping intervals that cover all the intervals in the input.
+// /**
+//  * @param {number[][]} intervals
+//  * @return {number[][]}
+//  */
+// var merge = function(intervals) {
+
+// };
+
 /**
- * @param {ListNode} head
- * @param {number} n
- * @return {ListNode}
+
+// var merge = function(intervals) {
+
+
+// };
+
+
+//#58. Length of Last Word
+
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+/**
+ * @param {string} s
+ * @return {number}
  */
-
-var removeNthFromEnd = function (head, n) {
-	//create a dummy node pointer for node before the head node (use dummy in adding or deleting a node)
-	let dummyHead = new ListNode(-1);
-    console.log(dummyHead, "dummyHead")
-	// create a head node
-	dummyHead.next = head;
-	let resultHead = dummyHead;
-    console.log(resultHead, "resultHead")
-	//create a counter to find the correct nth node
-	let counter = 0;
-    //create a tail poiinter that's set to the head
-	let tail = head;
-    console.log(tail, "tail")
-	while (counter < n) {
-        counter++
-		//increment the tail while counter < n
-		tail = tail.next;
-	}
-    //create a removedNode pointer that's set to head as well. when you get to n, remove the head
-	let removedNode = head;
-	let prev = dummyHead;
-    //increment 3 pointers until tail = null and see where it lands
-	while (tail) {
-		tail = tail.next;
-        removedNode = removedNode.next;
-        prev = prev.next;
-	}
-    prev.next = removedNode.next;
-    return resultHead.next;
+var lengthOfLastWord = function (s) {
+	//remove additional spaces with regex
+	//The trim() method removes whitespace from both ends of a string and returns a new string, without modifying the original string.
+	newString = s.trim();
+	let changed = newString.split(" ");
+	let last = changed.length - 1;
+	return changed[last].length;
 };
-//time complex: O(n);space O(1)
 
-console.log(removeNthFromEnd([1, 2, 3, 4, 5], 2));
+console.log(lengthOfLastWord("   fly me   to   the moon  ")); //4
