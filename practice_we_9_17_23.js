8586eeaaadb5f89aa648f1497a5e7b3ccf5f3409
Megha Sahgal <meghas@gmail.com>
2023-09-17 18:25:05 -0700

    LC #83

--- practice_we_9_17_23.js
+++ practice_we_9_17_23.js
@@ -32,7 +32,8 @@
 
 // console.log(heightChecker([1,1,4,2,1,3])) //3
 
-// //
+// //1071. Greatest Common Divisor of Strings
+
 // For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
 
 // Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
@@ -58,3 +59,24 @@ var gcdOfStrings = function(str1, str2) {
 };
 
 console.log(gcdOfStrings("ABCABC", "ABC"));///ABC
+
+
+// 83. Remove Duplicates from Sorted List
+
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once.
+// Return the linked list sorted as well.
+var deleteDuplicates = function (head) {
+	if (!head) return head;
+
+	const current = head;
+
+	while (head !== null) {
+		if (head.next !== null && head.val === head.next.val) {
+			head.next = head.next.next;
+		} else {
+			head = head.next;
+		}
+	}
+
+	return current;
+};
