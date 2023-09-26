ad46bee1b165b88f0387d88c5fac4e2972e4d3d4
Megha Sahgal <meghas@gmail.com>
2023-09-26 16:35:32 -0700

    LC #209

--- practice_we_9_17_23.js
+++ practice_we_9_17_23.js
@@ -192,3 +192,23 @@ var findKthLargest = function(nums, k) {
     return nums[k-1];
 
 };
+
+// #209
+// Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {boolean}
+//  */
+var containsNearbyDuplicate = function(nums, k) {
+    for (let i=0; i < nums.length; i++){
+         let j = nums.length-1
+         while(i < j){
+             if(nums[i] == nums[j] && Math.abs(i-j) <=k){
+                 return true;
+             }
+         j--
+         }
+    }
+
+    return false
+};
