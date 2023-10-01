c22f8758fe99e9f7ba4df2182427b98167a412bb
Megha Sahgal <meghas@gmail.com>
2023-09-30 19:35:56 -0700

    LC #228

--- practice_we_9_17_23.js
+++ practice_we_9_17_23.js
@@ -303,3 +303,39 @@ var invertTree = function(root) {
 
 
 };
+
+
+// #228 Summary Range
+// You are given a sorted unique integer array nums.
+
+// A range [a,b] is the set of all integers from a to b (inclusive).
+
+// Return the smallest sorted list of ranges that cover all the numbers in the array exactly.
+// That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+// Each range [a,b] in the list should be output as:
+
+// "a->b" if a != b
+// "a" if a == b
+var summaryRanges = function (nums) {
+	let res = [];
+	//define a default val
+	let newRange = true;
+	for (let i = 0; i < nums.length; i++) {
+		if (newRange) {
+			point = nums[i];
+			newRange = false;
+		}
+		//if no dups between adj numbers
+		if (nums[i + 1] != nums[i] + 1) {
+			//assign a new range and push it into the res array
+			newRange = true;
+			if (point != nums[i]) res.push(`${point}->${nums[i]}`);
+			//if there are adjacent dups push the first number
+			else res.push(`${nums[i]}`);
+		}
+	}
+	return res;
+};
+
+console.log(summaryRanges([0, 2, 3, 4, 6, 8, 9]));
