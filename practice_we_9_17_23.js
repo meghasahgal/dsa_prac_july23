0a6f11aecc6c5446803b490e1bb4b687a575e10a
Megha Sahgal <meghas@gmail.com>
2023-09-30 19:06:43 -0700

    LC 226 invert binary tree

--- practice_we_9_17_23.js
+++ practice_we_9_17_23.js
@@ -272,3 +272,34 @@ MyStack.prototype.top = function() {
 MyStack.prototype.empty = function() {
     return this.queue.length === 0;
 };
+
+
+/** LC
+ * 226. Invert Binary Tree
+
+Given the root of a binary tree, invert the tree, and return its root.
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if(!root) return null;
+  //Swap the left and right node using a temp variable
+  //Recursively call the function again for the left and right node
+
+  let temp = root.left
+  root.left = root.right
+  root.right = temp
+  if(root.left) invertTree(root.left)
+  if(root.right) invertTree(root.right)
+  return root;
+
+
+};
