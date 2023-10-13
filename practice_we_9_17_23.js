6376eca01ddfe984534d6a597bb0fe5e96f720c5
Megha Sahgal <meghas@gmail.com>
2023-10-13 14:57:50 -0700

    lc 70

--- practice_we_9_17_23.js
+++ practice_we_9_17_23.js
@@ -467,3 +467,22 @@ var solution = function(isBadVersion) {
 var divide = function(dividend, divisor) {
     return Math.min(Math.max(Math.trunc(dividend /  divisor),-(2**31)))
 };
+
+// 70. Climbing Stairs
+
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+let memo = [];
+var climbStairs = function (n) {
+	if (n == 1 || n == 2) {
+		return n;
+	}
+
+	if (memo[n]) {
+		return memo[n];
+	} else {
+		memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+		return memo[n];
+	}
+};
