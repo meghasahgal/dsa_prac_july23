e80202e12e77d8b00a9a0febba1c5e56bc9578f3
Megha Sahgal <meghas@gmail.com>
2023-10-15 18:55:46 -0700

    LC 141 LL cycle

--- practice_we_9_17_23.js
+++ practice_we_9_17_23.js
@@ -498,4 +498,17 @@ var merge = function (nums1, m, nums2, n) {
 	nums1.sort((a, b) => a - b);
 
 	return nums1;
+
+};
+
+
+// 141. Linked List Cycle
+var hasCycle = function (head) {
+	let fast = head;
+	while (fast && fast.next) {
+		head = head.next;
+		fast = fast.next.next;
+		if (head === fast) return true;
+	}
+	return false;
 };
